4ee223fa77667ef08a416a4a6ea566acf0adc8b3
Tuatarian <betterthangeowizard@gmail.com>
2022-06-26 04:19:40 -0700

    half-implemented goal rotation system

--- lvleditor.nim
+++ lvleditor.nim
@@ -29,7 +29,7 @@ var
     offset : Vector2
     mode : int
     lvout : string
-    lvn = 1
+    lvn = 2
     polys : seq[seq[Vector2]]
     polysCCW : seq[bool]
     drawnPolys : seq[seq[Vector2]]
@@ -49,6 +49,8 @@ var
     pDrawnPolys : seq[seq[Vector2]]
     lven = makevec2(PI, E)
     lvenParts : seq[Vector2]
+    lvenOmega : float
+    typingLvenOmega : bool
     sPos : Vector2
     closed : seq[bool]
 
@@ -81,18 +83,19 @@ proc loadLeveL(lvl : string) =
             polysCCW.add false
             omegas.add 0
             pivots.add screenCenter
+            closed.add true
             
             let terms = toSeq lvll[i].split(',').filterIt(it != "")
             let coords = terms[0].split(' ').filter(x => x != "").map(x => parseFloat x)
             for z in 0..<coords.len div 2:
                 polys[i].add makevec2(coords[2*z], coords[2*z + 1])
-                if z == 2:
-                    polysCCW[i] = isCCW polys[i]
+            polysCCW[i] = isCCW polys[i]
             omegas[i] = terms[1].parseFloat
             let pv = terms[2].split(" ").filter(x => x != "").toSeq.map(x => parseFloat x)
             pivots[i] = makevec2(pv[0], pv[1])
         elif disc == '/':
-            lven = lvll[i].split(' ').filter(x => x != "").map(x => parseFloat x).makevec2
+            # lvenOmega = lvll[i][0].parseInt.float
+            lven = lvll[i][1..^1].split(' ').filter(x => x != "").map(x => parseFloat x).makevec2
             echo lven
         elif disc == 's':
             sPos = lvll[i].split(' ').filter(x => x != "").map(x => parseFloat x).makevec2
@@ -153,16 +156,20 @@ while not WindowShouldClose():
             cObj = polys.len - 1
             mode = 0
 
-        if IsKeyPressed(KEY_BACKSPACE) and IsKeyDown(KEY_LEFT_ALT):
-            polys.delete cObj
-            drawnPolys.delete cObj
-            omegas.delete cObj
-            pivots.delete cObj
-            polysCCW.delete cObj
-            cObj = (cObj - 1).sgnmod(0, polys.len)
+        if IsKeyDown KEY_LEFT_ALT:
+            if IsKeyPressed KEY_BACKSPACE:
+                polys.delete cObj
+                drawnPolys.delete cObj
+                omegas.delete cObj
+                pivots.delete cObj
+                polysCCW.delete cObj
+                cObj = (cObj - 1).sgnmod(0, polys.len)
 
-        if IsKeyPressed KEY_L:
-            lven = screen2world mpos
+
+        elif IsKeyPressed KEY_L:
+            if mode == 3:
+                mode = 0
+            else: mode = 3
         if lven != makevec2(PI, E):
             lvenParts = updateParticles(lvenParts, numparts = 20, linspeed = 50, rotspeed = 0, rad = 150, killRange = 10)
 
@@ -219,6 +226,28 @@ while not WindowShouldClose():
                     polysCCW[cObj] = true
                 else:
                     polysCCW[cObj] = false
+        elif mode == 3:
+            if IsMouseButtonPressed MOUSE_LEFT_BUTTON:
+                lven = screen2world mpos
+            if IsKeyPressed KEY_O:
+                typingLvenOmega = not typingLvenOmega
+                if typingLvenOmega:
+                    if lvenOmega == 0:
+                        heldOmega = ""
+                    else:
+                        heldOmega = $lvenOmega
+                else:
+                    lvenOmega = parseFloat heldOmega
+                    heldOmega = ""
+            
+            if typingLvenOmega:
+                if heldOmega.len > 0 and IsKeyPressed KEY_BACKSPACE: heldOmega = heldOmega[0..^2]
+                else:
+                    let cPressed = GetCharPressed()
+                    if cPressed != 0 and char(cPressed) != 'o':
+                        heldOmega &= char cPressed
+                if heldOmega != "": lvenOmega = parseFloat heldOmega
+
         # Adding omegas and pivots to polys
 
         if typingOmega:
@@ -229,9 +258,9 @@ while not WindowShouldClose():
                 if cPressed != 0 and char(cPressed) != 'o':
                     heldOmega &= char cPressed
             if heldOmega != "": omegas[cObj] = parseFloat heldOmega
-        if IsKeyPressed(KEY_O):
+        if IsKeyPressed(KEY_O) and mode != 3:
             typingOmega = not typingOmega
-            if typingOmega == false: 
+            if not typingOmega: 
                 if heldOmega != "":
                     omegas[cObj] = parseFloat heldOmega
                     heldOmega = ""
@@ -279,8 +308,10 @@ while not WindowShouldClose():
 
         if lven != makevec2(E, PI):
             DrawRectangleV(world2screen(lven - makevec2(25, 25)), makevec2(50, 50)*zoom, C7)
+            if lvenOmega != 0:
+                drawTextCentered(heldOmega, lven.world2screen.x.int, lven.world2screen.y.int, int 60*zoom, RED)
             for i in 0..<lvenParts.len:
-                DrawRectangleV(world2screen(lvenParts[i] + lven), zoom*makevec2(12, 15), C7)
+                DrawRectangleV(world2screen(lvenParts[i] + lven), zoom*makevec2(15, 15), C7)
         
         if sPos != makevec2(E, PI):
             DrawRectangleV(world2screen sPos, makevec2(40, 40)*zoom, C1)
@@ -291,7 +322,7 @@ while not WindowShouldClose():
             lvout = ""
             for i in 0..<polys.len:
                 lvout &= "&" & ($polys[i]).replace(",", "").replace("x:", "").replace("y:", "").replace("(", "").replace(")", "")[3..^2] & &",{$omegas[i]}," & ($pivots[i]).replace("x:", "").replace("y:", "").replace("(", "").replace(")", "").replace(",", "")[1..^1] & "\n" # should have just used regex or some other kind of filtering
-            lvout &= "/" & ($lven).replace(",", "").replace("x:", "").replace("y:", "").replace("(", "").replace(")", "")[1..^1] & "\n"
+            lvout &= $"/" & ($lven).replace(",", "").replace("x:", "").replace("y:", "").replace("(", "").replace(")", "")[1..^1] & &",{$lvenOmega}," & "\n"
             lvout &= "s" & ($sPos).replace(",", "").replace("x:", "").replace("y:", "").replace("(", "").replace(")", "")[1..^1] & "\n"
             writeFile(&"lvl{$lvn}.txt", lvout)
             echo &"wrote to lvl{$lvn}.txt"
@@ -300,7 +331,7 @@ while not WindowShouldClose():
             pPolys[i] = pPolys[i].rotateVecSeq(degToRad omegas[i]/60, pivots[i])
             pDrawnPolys[i] = pPolys[i].mapIt(world2screen it)
             rlSetLineWidth 3
-            if closed[i] == true:
+            if closed[i]:
                 drawPolygon pDrawnPolys[i], C9, polysCCW[i]
             rlSetLineWidth 1
             DrawRectangleLines(int(-offset.x * zoom), int(-offset.y * zoom), int(1920 * zoom), int(1080 * zoom), WHITE)
